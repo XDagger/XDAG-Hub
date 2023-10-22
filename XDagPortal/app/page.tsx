b7c467b8a9faffb34fdb68403bd953ea4ad2701a
BourgesMind <luzetao@gmail.com>
2023-10-22 18:58:55 +0800

    dev2023102204

--- XDagPortal/app/page.tsx
+++ XDagPortal/app/page.tsx
@@ -3,11 +3,14 @@ import { allDApps, allCommunities } from 'contentlayer/generated'
 import Main from './Main'
 import GithubSlugger from "github-slugger";
 import { writeFileSync } from 'fs'
+import getConfig from 'next/config';
 
 // let tagCount = undefined as any;
-let tagCount: Record<string, number> = undefined;
+// let tagCount: Record<string, number> | undefined = undefined;
 
 export default async function Page() {
+
+	const { publicRuntimeConfig, serverRuntimeConfig } = getConfig();
 
 	const sortedPosts = sortPosts( allDApps ?? [] )
 	const posts = allCoreContent( sortedPosts )
@@ -15,31 +18,36 @@ export default async function Page() {
 	/**
 	 * Count the occurrences of all tags across blog posts and write to json file
 	 */
-	const createTagCount= ( allApps )=> {
+	const createTagCount = ( allApps ) => {
 		// const tagCount: Record<string, number> = {}
-		tagCount = {};
+		// tagCount = {};
+
+
+		console.log( publicRuntimeConfig.tagCount ); // 输出 10
+
+		// publicRuntimeConfig.tagCount = 20;
+
 		allApps.forEach( ( file ) => {
 				if ( file.tags && (file.draft !== true) ) {
 					file.tags.forEach( ( tag ) => {
 						const formattedTag = GithubSlugger.slug( tag )
-						if ( formattedTag in tagCount ) {
-							tagCount[ formattedTag ] += 1
+						if ( formattedTag in publicRuntimeConfig.tagCount ) {
+							publicRuntimeConfig.tagCount [ formattedTag ] += 1
 						} else {
-							tagCount[ formattedTag ] = 1
+							publicRuntimeConfig.tagCount [ formattedTag ] = 1
 						}
 					} )
 				}
 			}
 		)
 
-		console.log('.............',tagCount)
-		writeFileSync( './app/tag-data.json', JSON.stringify( tagCount ) )
+		console.log( '.............', publicRuntimeConfig.tagCount )
+		writeFileSync( './app/tag-data.json', JSON.stringify( publicRuntimeConfig.tagCount ) )
 	}
 
-	if( !tagCount ){
-		createTagCount(allCommunities);
+	if ( Object.keys( publicRuntimeConfig.tagCount ).length < 1 ) {
+		createTagCount( allCommunities );
 	}
-
 
 
 	return (
