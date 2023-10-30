4daf74467c1c022db0ca6a7aafb622b03778b454
BourgesMind <luzetao@gmail.com>
2023-10-30 22:39:49 +0800

    dev1030

--- XDagPortal/data/projectsData.ts
+++ XDagPortal/data/projectsData.ts
@@ -1,19 +1,18 @@
 const projectsData = [
   {
     title: 'Office Site',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    description: `XDAG is an avant-garde distributed ledger technology based on the directed acyclic graph infrastructure. 
+    It is the premier community-driven DAG+POW project that can be mined, with the major network launched in January 2018. `,
     imgSrc: '/static/images/OfficeSite.jpg',
     href: 'https://xdag.io/',
   },
   {
-    title: 'Chat Community',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'XDag Communities',
+    description: `There have been many exciting events along the development journey of XDAG. 
+    XDAG enthusiasts gather in various communication channels to share their perspectives, 
+    provide suggestions, and express their life experiences.`,
     imgSrc: '/static/images/XDagCommunity.png',
-    href: 'https://t.me/dagger_cryptocurrency',
+    href: '/dapp/doc/XDag-Communities',
   },
   {
     title: 'Different Wallets In XDag',
@@ -23,13 +22,14 @@ const projectsData = [
     So why did the community develop these wallets, and in which scenarios are they most suitable? 
     This article will cover these topics.`,
     imgSrc: '/static/images/differentWalletsInXDag.png',
-    href: '/dapp/wallet/different-wallets-in-XDag',
+    href: '/dapp/doc/different-wallets-in-XDag',
   },
   {
     title: 'Being a XDag Developer',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    description: `XDAG has an approximate maximum supply of 1.446294144 billion without any private placement, 
+    initial coin offering (ICO), or project sponsor. It represents a truly decentralized, efficient, secure, and fair sentiment, 
+    being developed and maintained by blockchain enthusiasts from around the world.
+    So, welcome to join us and become a vibrant developer.!`,
     imgSrc: '/static/images/XDagDeveloper.png',
     href: 'https://github.com/XDagger',
   },
